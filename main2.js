6e2b3aa55d54bc6b7ea572d1720b0b540722fd93
BPluijm <75165735+BPluijm@users.noreply.github.com>
2021-03-08 20:08:43 +0100

    Landen gekleurd

--- main2.js
+++ main2.js
@@ -1,29 +1,49 @@
 async function sortCountries () {
     try {
         const result = await axios.get("https://restcountries.eu/rest/v2/all");
-        const sorted = result.data.sort((country1, country2) => country1.population - country2.population)
-        console.log(sorted);
-        makeList(sorted);
+        const sortedCountries = result.data.sort((country1, country2) => country1.population - country2.population);
+        for (country of sortedCountries) {
+            const { name, flag, region } = country;
+            makeList(name, flag, region);
+        }
+
     } catch (error) {
         console.log(error);
     }
 }
-
-function makeList(array) {
-    const list = document.querySelector('.list');
-    for (let i = 0; i < array.length; i++) {
+    function makeList(countryName, countryFlag, countryRegion) {
+        const list = document.querySelector('.list');
         let country = document.createElement('li')
         let name = document.createElement('span');
         let flag = document.createElement('img');
         name.classList.add('country-name'); // = hetzelfde als setAttribute
         flag.classList.add('country-flag'); // = hetzelfde als setAttribute
-        name.textContent = array[i].name;
-        flag.setAttribute('src', array[i].flag);
+        name.textContent = countryName;
+        colorTheCountries(name, countryRegion)
+        flag.setAttribute('src', countryFlag);
         country.appendChild(flag);
         country.appendChild(name);
         list.appendChild(country);
     }
 
+    function colorTheCountries(nameElement, countryRegion) {
+        switch (countryRegion) {
+            case 'Africa':
+                nameElement.classList.add('africa');
+                break
+            case 'Americas':
+                nameElement.classList.add('americas');
+                break
+            case 'Asia':
+                nameElement.classList.add('asia');
+                break
+            case 'Europe':
+                nameElement.classList.add('europe');
+                break
+            case 'Oceania':
+                nameElement.classList.add('oceania');
+                break
+        }
 }
 
 sortCountries();
